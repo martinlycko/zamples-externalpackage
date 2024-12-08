16cf61316640f73c8b219fcf80525bef858c0932
mlycko <martin@mlycko.com>
2024-12-08 10:57:47 +0000

    Added new function, removed exe

--- src/root.zig
+++ src/root.zig
@@ -5,6 +5,10 @@ pub export fn add(a: i32, b: i32) i32 {
     return a + b;
 }
 
+pub fn double(a: i32) i32 {
+    return a * 2;
+}
+
 test "basic add functionality" {
     try testing.expect(add(3, 7) == 10);
 }
